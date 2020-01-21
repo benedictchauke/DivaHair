31b8ae314a79a139b849e3039d366009851a218a
benedictchauke <48420109+benedictchauke@users.noreply.github.com>
2020-01-21 21:24:08 +0200

    adding events

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,1 +1,10 @@
-﻿alert("hello billionaire");
+﻿var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("Buy");
+button.addEventListener("click", function () {
+    alert("Buying Item");
+});
+
+var info = document.getElementByClassName("prop");
+var listItems = info.item[0].children;
