26483f0c675e70b4be06011b9a6ade7a9393122e
benedictchauke <48420109+benedictchauke@users.noreply.github.com>
2020-02-10 21:20:13 +0200

    creating a repository

---
+++ Data/HairRepo.cs
@@ -0,0 +1,39 @@
+﻿using DivaHair.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivaHair.Data
+{
+    public class HairRepo : IHairRepo
+    {
+        private readonly HairContext _ctx;
+
+        public HairRepo(HairContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Product>GetAllProducts()
+        {
+            return _ctx.Products
+                       .OrderBy(p => p.Size)
+                       .ToList();
+
+        }
+
+        public IEnumerable<Product>GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                       .Where(p => p.Category == category)
+                       .ToList(); 
+        }
+        
+        public bool SaveAll()
+        {
+            return _ctx.SaveChanges() > 0; 
+        }
+     }
+}
---
+++ Data/IHairRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DivaHair.Data.Entities;
+
+namespace DivaHair.Data
+{
+    public interface IHairRepo
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetProductsByCategory(string category);
+
+        bool SaveChanges();
+    }
+}
