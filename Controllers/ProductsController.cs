2478c2f8074d98f79c0fb661853312467eb01ef3
benedictchauke <48420109+benedictchauke@users.noreply.github.com>
2020-02-11 22:15:25 +0200

    creating an api controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using DivaHair.Data;
+using DivaHair.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivaHair.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IHairRepo _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IHairRepo repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch(Exception exc)
+            {
+                _logger.LogError("$Failed to get products: {exc}");
+                return Json("Failed to get products");
+            }
+            
+        }
+        
+    }
+}
